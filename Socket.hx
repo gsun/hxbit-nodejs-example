e26045b81bff283c642abae971f40de0cdf877b6
gsun <sun_jungang@hotmail.com>
2017-01-17 14:06:38 +0800

    first draft

---
+++ Socket.hx
@@ -0,0 +1,153 @@
+import js.node.Buffer;
+import js.node.net.Socket;
+import js.node.net.Server;
+import js.node.Net;
+
+private class SocketInput extends haxe.io.BytesInput {
+
+    public function new(b:haxe.io.Bytes) {
+		super(b);
+		this.bigEndian = true;
+	}
+	public var available(get, never) : Int;
+
+	function get_available() {
+		return length-position;
+	}
+}
+
+
+private class SocketOutput extends haxe.io.Output {
+	var s : js.node.net.Socket;
+
+	public function new(s) {
+		this.s = s;
+	}
+	
+	public function wait() {
+		s.cork();
+    }
+	
+	override public function flush() {
+		s.uncork();
+    }
+
+	override function writeByte( c : Int ) {
+		var bf = Buffer.alloc(1);
+		bf.fill(c);
+		s.write(bf);
+	}
+
+	override function write( b : haxe.io.Bytes )  {
+	    var bf = Buffer.hxFromBytes(b);
+		s.write(bf);		
+	}
+	
+	override function writeBytes( b : haxe.io.Bytes, pos : Int, len : Int ) : Int {
+		if( len > 0 ) {
+			var bf = Buffer.hxFromBytes(b);
+			s.write(bf);
+		}
+		return len;
+	}
+
+	override function writeInt32( i : Int ) {
+		var bf = Buffer.alloc(4);
+		bf.writeInt32BE(i,0);
+		s.write(bf);
+	}
+
+	override function writeString( str : String ) {
+		var bf = Buffer.from(str);
+		s.write(bf);
+	}
+
+}
+
+class Socket {
+
+	static var openedSocks = [];
+	var client : Bool;
+	var s : js.node.net.Socket;
+    var serv : Server;
+	
+	public var out(default, null) : SocketOutput;
+	public var input(default, null) : SocketInput;
+	public var timeout(default, set) : Null<Float>;
+
+	public function new() {
+		client = true;
+	}
+
+	public function set_timeout(t:Null<Float>) {
+		if( s != null ) s.setTimeout(t == null ? 0x7FFFFFFF : Math.ceil(t * 1000));
+		return this.timeout = t;
+	}
+
+	public function connect( host : String, port : Int, onConnect : Void -> Void ) {
+		s = new js.node.net.Socket();
+		s.connect({host:host,port:port}, function() {
+			out = new SocketOutput(s);
+			bindEvents();
+			onConnect();
+		});
+	}
+
+	function bindEvents() {
+        s.on(SocketEvent.Error, function(e:js.Error) {
+			onError(e.message);
+		});
+		s.on(SocketEvent.Data, function(data:Buffer) {
+			input = new SocketInput(data.hxToBytes());			
+            onData();
+        });
+        s.on(SocketEvent.End, function() {
+			close();
+			onError("Closed");
+		});
+	}
+	
+	public static inline var ALLOW_BIND = true;
+
+	public function bind( host : String, port : Int, onConnect : Socket -> Void, listenCount = 5 ) {
+		client = false;
+		close();
+		openedSocks.push(this);
+		serv = Net.createServer(function(sock) {
+			var s = new Socket();
+			s.s = sock;
+			s.bindEvents();
+			s.out = new SocketOutput(sock);
+			openedSocks.push(s);
+			onConnect(s);
+		});
+		try serv.listen(port, host) catch( e : Dynamic ) {
+			close();
+			throw e;
+		};
+		serv.maxConnections = listenCount;
+	}
+
+	public function close() {
+		openedSocks.remove(this);
+		if (client) {
+			if( s != null ) {
+				try s.end() catch( e : Dynamic ) { };
+				s = null;
+			}	
+		} else {
+			if( serv != null ) {
+				try serv.close() catch( e : Dynamic ) { };
+				serv = null;
+			}			
+		}
+	}
+
+	public dynamic function onError(msg:String) {
+		throw "Socket Error " + msg;
+	}
+
+	public dynamic function onData() {
+	}
+
+}
---
+++ Test.hx
@@ -0,0 +1,131 @@
+
+class Cursor implements hxbit.NetworkSerializable {
+
+	@:s var color : Int;
+	@:s public var uid : Int;
+	@:s public var x(default, set) : Float;
+	@:s public var y(default, set) : Float;
+
+	var net : Test;
+
+	public function new( color, uid=0 ) {
+		this.color = color;
+		this.uid = uid;
+		init();
+		x = 0;
+		y = 0;
+	}
+
+	public function networkGetOwner() {
+		return this;
+	}
+
+	function set_x( v : Float ) {
+		if( v == x ) return v;
+		return this.x = v;
+	}
+
+	function set_y( v : Float ) {
+		return this.y = v;
+	}
+
+	public function toString() {
+		return "Cursor " + StringTools.hex(color, 6)+(enableReplication?":ALIVE":"");
+	}
+
+	function init() {
+		net = Test.inst;
+		
+
+		enableReplication = true;
+		
+		net.log("Init "+this);
+
+	}
+
+	@:rpc function blink( s : Float ) {
+	}
+
+	public function alive() {
+		init();
+		// refresh bmp
+		this.x = x;
+		this.y = y;
+		if( uid == net.uid ) {
+			net.cursor = this;
+			net.host.self.ownerObject = this;
+		}
+		
+		net.log("alive "+this);
+	}
+
+}
+
+//PARAM=-lib hxbit
+class Test {
+
+	static var HOST = "127.0.0.1";
+	static var PORT = 6676;
+
+	public var host : SocketHost;
+	//public var event : hxd.WaitEvent;
+	public var uid : Int;
+	public var cursor : Cursor;
+
+	public function init() {
+		//event = new hxd.WaitEvent();
+		host = new SocketHost();
+		host.setLogger(function(msg) log(msg));
+
+		#if server
+		host.wait(HOST, PORT, function(c) {
+			log("Client Connected");
+		});
+		host.onMessage = function(c,uid:Int) {
+			log("Client identified ("+uid+")");
+			var cursorClient = new Cursor(0x0000FF, uid);
+			c.ownerObject = cursorClient;
+			c.sync();
+		};
+		log("Server Started");
+
+		start();
+
+		#else
+		// we could not start the server
+		log("Connecting");
+
+		uid = 1 + Std.random(1000);
+		host.connect(HOST, PORT, function(b) {
+			if( !b ) {
+				log("Failed to connect to server");
+				return;
+			}
+			log("Connected to server");
+			host.sendMessage(uid);
+		});			
+		#end	
+
+		
+	}
+
+	public function log( s : String, ?pos : haxe.PosInfos ) {
+		pos.fileName = (host.isAuth ? "[S]" : "[C]") + " " + pos.fileName;
+		haxe.Log.trace(s, pos);
+	}
+
+	function start() {
+		cursor = new Cursor(0xFF0000);
+		log("Live");
+		host.makeAlive();
+	}
+
+	public static var inst : Test;
+	static function main() {
+		inst = new Test();
+		inst.init();
+	}
+	
+	public function new() {}
+
+}
