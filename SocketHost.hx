616aa262c2547d3332e83ef8f35a3cf11afde354
Gang Sun <sun_jungang@hotmail.com>
2019-11-27 10:00:28 +0800

    replace tab with space

--- SocketHost.hx
+++ SocketHost.hx
@@ -24,104 +24,104 @@ import hxbit.NetworkHost;
 
 class SocketClient extends NetworkClient {
 
-	var socket : Socket;
+    var socket : Socket;
 
-	public function new(host, s) {
-		super(host);
-		this.socket = s;
-		if( s != null )
-			s.onData = function() {
-				// process all pending messages
-				trace("input length", socket.input.available);
-				while( readData(socket.input, socket.input.available) ) {
-				}
-			}
-	}
+    public function new(host, s) {
+        super(host);
+        this.socket = s;
+        if( s != null )
+            s.onData = function() {
+                // process all pending messages
+                trace("input length", socket.input.available);
+                while( readData(socket.input, socket.input.available) ) {
+                }
+            }
+    }
 
-	override function error(msg:String) {
-		socket.close();
-		super.error(msg);
-	}
+    override function error(msg:String) {
+        socket.close();
+        super.error(msg);
+    }
 
-	override function send( bytes : haxe.io.Bytes ) {
-		socket.out.wait();
-		socket.out.writeInt32(bytes.length);
-		socket.out.write(bytes);
-		socket.out.flush();
-	}
+    override function send( bytes : haxe.io.Bytes ) {
+        socket.out.wait();
+        socket.out.writeInt32(bytes.length);
+        socket.out.write(bytes);
+        socket.out.flush();
+    }
 
-	override function stop() {
-		super.stop();
-		if( socket != null ) {
-			socket.close();
-			socket = null;
-		}
-	}
+    override function stop() {
+        super.stop();
+        if( socket != null ) {
+            socket.close();
+            socket = null;
+        }
+    }
 
 }
 
 class SocketHost extends NetworkHost {
 
-	var connected = false;
-	var socket : Socket;
-	public var enableSound : Bool = true;
+    var connected = false;
+    var socket : Socket;
+    public var enableSound : Bool = true;
 
-	public function new() {
-		super();
-		isAuth = false;
-	}
+    public function new() {
+        super();
+        isAuth = false;
+    }
 
-	override public function dispose() {
-		close();
-		if( NetworkHost.current == this ) NetworkHost.current = null;
-	}
+    override public function dispose() {
+        close();
+        if( NetworkHost.current == this ) NetworkHost.current = null;
+    }
 
-	function close() {
-		if( socket != null ) {
-			socket.close();
-			socket = null;
-		}
-		connected = false;
-	}
+    function close() {
+        if( socket != null ) {
+            socket.close();
+            socket = null;
+        }
+        connected = false;
+    }
 
-	public function connect( host : String, port : Int, ?onConnect : Bool -> Void ) {
-		close();
-		isAuth = false;
-		socket = new Socket();
-		socket.onError = function(msg) {
-			if( !connected ) {
-				socket.onError = function(_) { };
-				onConnect(false);
-			} else
-				throw msg;
-		};
-		self = new SocketClient(this, socket);
-		socket.connect(host, port, function() {
-			connected = true;
-			if( host == "127.0.0.1" ) enableSound = false;
-			clients = [self];
-			onConnect(true);
-		});
-	}
+    public function connect( host : String, port : Int, ?onConnect : Bool -> Void ) {
+        close();
+        isAuth = false;
+        socket = new Socket();
+        socket.onError = function(msg) {
+            if( !connected ) {
+                socket.onError = function(_) { };
+                onConnect(false);
+            } else
+                throw msg;
+        };
+        self = new SocketClient(this, socket);
+        socket.connect(host, port, function() {
+            connected = true;
+            if( host == "127.0.0.1" ) enableSound = false;
+            clients = [self];
+            onConnect(true);
+        });
+    }
 
-	public function wait( host : String, port : Int, ?onConnected : NetworkClient -> Void ) {
-		close();
-		isAuth = false;
-		socket = new Socket();
-		self = new SocketClient(this, null);
-		socket.bind(host, port, function(s) {
-			var c = new SocketClient(this, s);
-			pendingClients.push(c);
-			s.onError = function(_) c.stop();
-			if( onConnected != null ) onConnected(c);
-		});
-		isAuth = true;
-	}
+    public function wait( host : String, port : Int, ?onConnected : NetworkClient -> Void ) {
+        close();
+        isAuth = false;
+        socket = new Socket();
+        self = new SocketClient(this, null);
+        socket.bind(host, port, function(s) {
+            var c = new SocketClient(this, s);
+            pendingClients.push(c);
+            s.onError = function(_) c.stop();
+            if( onConnected != null ) onConnected(c);
+        });
+        isAuth = true;
+    }
 
-	public function offlineServer() {
-		close();
-		self = new SocketClient(this, null);
-		isAuth = true;
-	}
+    public function offlineServer() {
+        close();
+        self = new SocketClient(this, null);
+        isAuth = true;
+    }
 
 }
